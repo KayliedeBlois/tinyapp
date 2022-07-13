7b85999da07e2914eece29f054f975a8d18c0008
KayliedeBlois <kayliedeblois@gmail.com>
2022-07-12 23:58:44 -0400

    Updated login/logout button

--- express_server.js
+++ express_server.js
@@ -13,6 +13,8 @@ charactersLength));
 const { response } = require("express");
 //once new ID is generated, add to data base "id": longURL (key value pair)
 
+const cookieParser = require("cookie-parser");
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -21,13 +23,16 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cookieParser());
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
 app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase };
+  console.log(req.cookies.username);
+  const templateVars = {username: req.cookies.username, urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
@@ -48,11 +53,12 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
+  const templateVars = {username: req.cookies.username};
   res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { username: req.cookies.username, id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 app.post("/urls", (req, res) => {
@@ -86,7 +92,11 @@ app.post("/urls/:id/update", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  console.log(req.body.username);
   res.cookie('username', req.body.username);
   res.redirect('/urls/');
 });
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect('urls');
+})
